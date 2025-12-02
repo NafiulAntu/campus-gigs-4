549fd6a0683a7a68deeca3e56540366ec7b00ca0
NafiulAntu <nafiul.nia@gmail.com>
2025-12-02 23:48:43 +0600

    Revert notifications to previous style without accept/reject buttons

--- FrontEnd/src/components/Post/sidebar/notifications.jsx
+++ FrontEnd/src/components/Post/sidebar/notifications.jsx
@@ -56,9 +56,7 @@ export default function Notifications({ onBack }) {
           read: notif.is_read,
           icon: getIcon(notif.type),
           color: getColor(notif.type),
-          link: notif.link,
-          actorId: notif.actor_id, // Store actor_id for accept/reject actions
-          data: notif.data
+          link: notif.link
         }));
         setNotifications(apiNotifications);
       }
